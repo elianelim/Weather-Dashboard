d7bc2749d0c3a5ef91d6422ca86114beb79259f8
elianelim <elianelim.sf@gmail.com>
2023-01-28 16:50:18 +0800

    staring codes for JS

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
-//Assigning API key to const 
-const APIKey = "822415cdef222fbb18ec1a2abbf9c0bc";
-var
+var searchInput = document.querySelector('#searchInput');
 
 function searchCity() {
-
+    var searchInput = getElementById("searchInput");
+    var searchValue = searchInput.Value;
+    console.log(searchValue);
 }
+
+
+
+
+//Assigning API key to const 
+const APIKey = "822415cdef222fbb18ec1a2abbf9c0bc";
