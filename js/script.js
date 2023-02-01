70482d083531afc3f595c481f5f004f58654e45f
elianelim <elianelim.sf@gmail.com>
2023-02-01 09:29:22 +0800

    started index2 with new codes

--- js/script.js
+++ js/script.js
@@ -1,13 +1,10 @@
-var searchInput = document.querySelector('#searchInput');
-
-function searchCity() {
-    var searchInput = getElementById("searchInput");
-    var searchValue = searchInput.Value;
-    console.log(searchValue);
-}
-
+var searchInput = document.getElementById('#searchInput');
+var searchButtonEl = document.getElementById('#search-button');
 
 
 
 //Assigning API key to const 
 const APIKey = "822415cdef222fbb18ec1a2abbf9c0bc";
+
+searchButtonEl.addEventListener("click", clearCards);
+console.log("hello")
